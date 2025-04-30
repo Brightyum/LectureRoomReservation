5454245086989f7a4b5be1b9cb3d320abf20fa93
Yum <duatmddnr@gmail.com>
2025-04-30 23:31:21 +0900

    devleop: UserManagement클래스 테스트 코드 일부 작성

--- src/main/java/admin/UserManagement.java
+++ src/main/java/admin/UserManagement.java
@@ -3,10 +3,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package admin;
+
 import db.Excel;
 import java.io.IOException;
 import java.util.List;
 import java.util.*;
+
 //import java.util.ArrayList;
 /**
  *
@@ -20,16 +22,27 @@ public class UserManagement implements Admin {
     private Scanner sc;
     private int userNoticeIdx;
     
-    public UserManagement() throws IOException{
-        this.excel = new Excel();
-        this.userNameIdx = 0;
-        this.getUserInformation();
-        this.sc = new Scanner(System.in);
-        this.userNoticeIdx = 4;
+    public UserManagement() {
+        this(createDefaultExcel(), new Scanner(System.in));
     }
     
-    @Override
-    public void getUserInformation() {
+    public UserManagement(Excel excel, Scanner sc) {
+        this.excel = excel;
+        this.userNameIdx = 0;
+        this.userNoticeIdx = 4;
+        this.sc = sc;
+        this.getUserInformation();
+    }
+    
+    private static Excel createDefaultExcel() {
+        try {
+            return new Excel();
+        } catch (IOException ex) {
+            System.out.println("Excel 생성 실패" + ex);
+            return null;
+        }
+    }
+    private void getUserInformation() {
         this.excel.readUser();
         userInfo = this.excel.getUserInfo();
         
